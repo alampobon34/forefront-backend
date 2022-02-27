d04b20adc6365763382d65d0861f3d504a51b73e
Sayed Monshurul Alam <75158575+alampobon34@users.noreply.github.com>
2022-02-28 03:36:07 +0600

    api created

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+class ContactController extends Controller
+{
+    public function addContact(Request $request){
+
+        $contact = new Contact;
+        $contact->name =$request->input('name');
+        $contact->email =$request->input('email');
+        $contact->subject =$request->input('subject');
+        $contact->message =$request->input('message');
+        $contact->save();
+        return $contact;
+    }
+}
---
+++ app/Http/Controllers/JoinNowController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class JoinNowController extends Controller
+{
+    public function joinNow(){
+        return "helloo";
+    }
+}
