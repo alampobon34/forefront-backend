e73a72072ae82393be126ac0555c132ca0dc9584
Sayed Monshurul Alam <75158575+alampobon34@users.noreply.github.com>
2022-02-28 16:17:58 +0600

    contact api done

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,16 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Contact;
+use Illuminate\Support\Facades\Validator;
 class ContactController extends Controller
 {
     public function addContact(Request $request){
 
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:3',
+            'email' => 'required|email',
+            'subject' => 'required|min:3',
+            'message' => 'required|min:5',
+        ]);
+
+        if($validator-> fails())
+        {
+            return response()->json([
+                'status' => 400,
+                'error' => $validator->messages(),
+            ]);
+        }
+
         $contact = new Contact;
-        $contact->name =$request->input('name');
-        $contact->email =$request->input('email');
-        $contact->subject =$request->input('subject');
-        $contact->message =$request->input('message');
+        $contact->name = $request->input('name');
+        $contact->email = $request->input('email');
+        $contact->subject = $request->input('subject');
+        $contact->message = $request->input('message');
         $contact->save();
-        return $contact;
+        return response()->json([
+            'status' => 200,
+            'success' => 'Contact Added Successfully!!',
+        ]);
+
+
+
     }
 }
